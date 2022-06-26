dd4e926e9c215e9a847d9b7d88d5e6690c21a49d
Roger <y232gu@uwaterloo.ca>
2022-06-26 15:28:16 -0400

    add test deck (#13)
    Demo_only: add test deck

--- Drafts/D3/Awake/core/src/com/cs446/awake/Awake.kt
+++ Drafts/D3/Awake/core/src/com/cs446/awake/Awake.kt
@@ -25,19 +25,17 @@ class Awake : Game() {
     init {
         game = this
         // shouldn't be here, only in start code for demo
-        val deck = Deck()
-        for (i in 1..5) {
-            deck.addCard(Card("empty", "card_empty.png", "use"))
-        }
+        val deck1 = getTestDeck()
+        val deck2 = getTestDeck()
         val states : MutableList<State> = mutableListOf()
-        val player = Player("Hero",100, 10, 10, deck, states)
+        val player = Player("Hero",100, 10, 10, deck1, states)
 
         player.updateState(State("Burn", 3, player))
         player.updateState(State("Freeze", 3, player))
         player.updateState(State("Poison", 3, player))
 
         val imgs = Array<String?>(arrayOf("skeleton1.png","skeleton2.png","skeleton3.png","skeleton2.png"))
-        val enemy = Enemy(imgs,"Enemy",999, 99, 99, deck, states)
+        val enemy = Enemy(imgs,"Enemy",999, 99, 99, deck2, states)
         board = Board(player, enemy, player, 1)
 
     }
---
+++ Drafts/D3/Awake/core/src/com/cs446/awake/model/testDeck.kt
@@ -0,0 +1,32 @@
+package com.cs446.awake.model
+import com.badlogic.gdx.utils.Array
+
+// return a test deck
+fun getTestDeck(): Deck {
+    val testDeck = Deck()
+    var i = 1
+    while (i <= 5){
+        // make 5 - i attack cards with damage i
+        var j = i
+        while (j < 5) {
+            val attackCard = ActionCard(("AttackCard$i$j"),"img.jpg", "Deals $i damage after costing ${j/2} strength", 0, j/2, i, Array<State>(0))
+            testDeck.addCard(attackCard)
+            j ++
+        }
+        // make a card that restores i health
+        val restoreCard = ActionCard("RestoreCard$i", "img.jpg", "Restores $i health after costing $i energy", i,0, (0-i), Array<State>(0))
+        testDeck.addCard(restoreCard)
+        j ++
+    }
+    // make some state cards
+    i = 1
+    while (i < 4){
+        val stateCard1 = ActionCard("burner$i", "img.jpg", "burns target for $i rounds and paralysis target for 1 round",
+            i, 0, 0, Array<State>(arrayOf(State("Burn", i),State("Paralysis", 1))))
+        testDeck.addCard(stateCard1)
+        val stateCard2 = ActionCard("freezer", "img.jpg", "freezes target for $i rounds",
+            i-1, 0, 0, Array<State>(arrayOf(State("Freeze", i),)))
+        testDeck.addCard(stateCard2)
+    }
+    return testDeck
+}
