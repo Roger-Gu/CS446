12519e9df9cd941f5481d21d8e1b2dc69a1f5b66
EzPzLetMeFreezie <huaxia20010418@gmail.com>
2022-06-26 23:48:52 -0400

    add drag highlight

--- Drafts/D3/Awake/core/src/com/cs446/awake/ui/BattleScreen.kt
+++ Drafts/D3/Awake/core/src/com/cs446/awake/ui/BattleScreen.kt
@@ -2,6 +2,7 @@ package com.cs446.awake.ui
 
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.Texture
+import com.badlogic.gdx.scenes.scene2d.ui.Image
 import com.cs446.awake.model.Board
 import com.cs446.awake.utils.BaseActor
 import com.cs446.awake.utils.BaseScreen
@@ -37,6 +38,10 @@ class BattleScreen(private val board: Board) : BaseScreen(){
         enemy.centerAtPosition(wid/2, height)
         enemy.moveBy(0f,-550f)
 
+        // Border for card
+        val borderTexture = Texture(Gdx.files.internal("highlight_border.png")) // TODO: change the texture
+        val borderImage = Image(borderTexture)
+
         val cardImg = Texture("card_empty.png")
         val cardWidth = cardImg.width.toFloat()
         var intervalWid = 40f
@@ -49,9 +54,23 @@ class BattleScreen(private val board: Board) : BaseScreen(){
             cardActor.moveBy((wid-(cardTotal*cardWidth + (cardTotal-1)*intervalWid))/2 + handIndex*cardWidth,0f)
             cardActor.setOnDropIntersect {
                 println("intersected when dropped")
+                cardActor.remove()
+                borderImage.remove()
             }
             cardActor.setOnDropNoIntersect {
                 cardActor.setPosition(cardActor.startX, cardActor.startY)
+                borderImage.remove()
+            }
+            cardActor.setOnDragIntersect {
+                //println("CARD USING?")
+                val borderWidth = 30
+                borderImage.setSize( cardActor.width + borderWidth * 2, cardActor.height + borderWidth * 2)
+                borderImage.setPosition( cardActor.x - borderWidth, cardActor.y - borderWidth )
+                stage.addActor(borderImage)
+                cardActor.toFront()
+            }
+            cardActor.setOnDragNoIntersect {
+                borderImage.remove()
             }
         }
 
