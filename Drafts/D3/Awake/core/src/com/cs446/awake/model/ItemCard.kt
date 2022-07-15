44467fbc509e180c9de86f600e7a893bf7eeea0a
Roger <y232gu@uwaterloo.ca>
2022-07-14 20:21:54 -0400

    dungeon view (#26)
    Roger- dungeon backend
    
    add dungeon backend
    define Data class that draws random item
    define global shared data

--- Drafts/D3/Awake/core/src/com/cs446/awake/model/ItemCard.kt
+++ Drafts/D3/Awake/core/src/com/cs446/awake/model/ItemCard.kt
@@ -1,5 +1,5 @@
 package com.cs446.awake.model
 
-class MaterialCard (cardName: String, img: String, usage: String, ) : Card(cardName, img, usage)
+class ItemCard (cardName: String, img: String, usage: String) : Card(cardName, img, usage)
 {
 }
--- Drafts/D3/Awake/core/src/com/cs446/awake/model/MaterialClass.kt
+++
@@ -1,6 +0,0 @@
-package com.cs446.awake.model
-
-class MaterialClass (cardName: String, img: String, usage: String) : Card(cardName, img, usage)
-{
-    override fun use(){}
-}
