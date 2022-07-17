515e5321efc437338bae12d4082031dc795716ac
Eric Liu <e44liu@uwaterloo.ca>
2022-07-17 01:28:41 -0400

    change parameter to start with lower case

--- Drafts/Awake/core/src/com/cs446/awake/model/Event.kt
+++ Drafts/Awake/core/src/com/cs446/awake/model/Event.kt
@@ -7,7 +7,7 @@ import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.Array
 
 
-open class Event(val backImg: String, val FrontImg: String) {
+open class Event(val backImg: String, val frontImg: String) {
     private var flipped: Boolean = false
     // return false if not activated
     fun isFlipped(): Boolean {
